e15d7bd74d602a841aee3e40805170fce56c4129
Sovello <sovellohpmgani@gmail.com>
2014-06-22 21:03:44 +0200

    Tried to implement the makeCacheMatrix, not tested yet

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 ##This is a function that creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        ##Initialize the inverse of this matrix to NULL (for memory concerns here)
+        matrixInverse <- NULL
+        
+        ##This function is used to access the matrix we are working with
+        
+        ##Defining a function to get inverse
+        calculateInverse <- function(){
+                matrixInverse <<- solve(x)
+        }
+        
+        ##This function is used to return the value of the mean
+        getInverse <- function(){
+                matrixInverse
+        }
 }
 
 
@@ -14,5 +27,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ##If the inverse is already calculated and the matrix has not changed
 ##then cacheSolve retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ##Get the inverse if at all it exists
+        inverse <- x$getInverse()
+        
+        ##If we found the inverse, we return it, else we go calculate it
+        if(!is.null(inverse)){
+                message("Retrieving the cached inverse")
+                return(inverse)
+        }
+        
+        theMatrix <- x$get
+        
 }
